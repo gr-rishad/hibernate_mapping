796d6bd4a3604732e1116f34a1b0adfcc3b2a176
gr-rishad <gr.rishad@gmail.com>
2022-07-03 10:06:52 +0600

    oneToOne completed

--- src/main/java/com/hibernate_mapping/OneToOne/controller/InstructorController.java
+++ src/main/java/com/hibernate_mapping/OneToOne/controller/InstructorController.java
@@ -1,14 +1,11 @@
 package com.hibernate_mapping.OneToOne.controller;
 
 import com.hibernate_mapping.OneToOne.entity.Instructor;
-import com.hibernate_mapping.OneToOne.entity.InstructorDetails;
 import com.hibernate_mapping.OneToOne.service.InstructorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/instructor")
@@ -22,8 +19,14 @@ public class InstructorController {
     }
 
     @PostMapping("/saveInstructor")
-    public ResponseEntity<Instructor> createInstructor(@RequestBody Instructor instructor){
-        Instructor instructor1= instructorService.saveOrUpdate(instructor);
+    public ResponseEntity<Instructor> createInstructor(@RequestBody Instructor instructor) {
+        Instructor instructor1 = instructorService.saveOrUpdate(instructor);
         return ResponseEntity.ok(instructor1);
+    }
+
+    @DeleteMapping("/deleteBy/{instructorId}")
+    public ResponseEntity<?> deleteInstructor(@PathVariable("instructorId") Long instructorId) {
+        instructorService.delete(instructorId);
+        return new ResponseEntity<>("Delete Successfully.", HttpStatus.OK);
     }
 }
